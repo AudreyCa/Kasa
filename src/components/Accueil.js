2f52074ddacc06c6208739092d0b5c7b90ba92dd
Aud Can <a.cannesson5@gmail.com>
2024-02-09 12:01:41 +0100

    first commit

---
+++ src/components/Accueil.js
@@ -0,0 +1,12 @@
+//import logo from '../../logo.svg';
+import '../styles/Accueil.css';
+import './App.css';
+
+function Accueil() {
+  return (
+    <div className="Accueil">
+    </div>
+  );
+}
+
+export default Accueil;
